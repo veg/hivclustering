7c082ffd43f4b7f137aa2d1f7480d991f67f15d6
Steven Weaver <sweaver@ucsd.edu>
2015-07-22 16:05:03 -0700

    repackaging hivnetworkcsv

--- lib/hivclustering/__init__.py
+++
@@ -1,6 +0,0 @@
-__version__ = '0.2'
-
-from .mtnetwork import *
-
-__all__ = []
-__all__ += mtnetwork.__all__
--- setup.py
+++ setup.py
@@ -8,23 +8,22 @@ from setuptools import setup
 sys.path.insert(0, join(split(abspath(__file__))[0], 'lib'))
 
 setup(name='hivclustering',
-      version="0.1.2",
+      version="0.1.3",
       description='HIV molecular clustering tools',
       author='Sergei Kosakovsky Pond',
       author_email='spond@ucsd.edu',
       url='http://github.com/veg/hivclustering',
       license='MIT License',
       packages=['hivclustering'],
-      package_dir={'': 'lib'},
       package_data={'hivclustering': [
             'data/HBL/*.bf',
     ]},
-    entry_points= {
-        'console_scripts': [
-            'hivnetworkcsv = hivclustering.hivnetworkcsv:main',
-            'tns = hivclustering.TNS:main',
-        ]
+    package_dir={
+        'networkbuild': 'hivclustering/networkbuild',
     },
+    scripts=[
+        'scripts/hivnetworkcsv',
+    ],
     dependency_links = ['git+git://github.com/veg/hyphy-python.git@0.1.1#egg=HyPhy-0.1.1',
                         'git+git://github.com/veg/BioExt.git@0.17.2#egg=BioExt-0.17.2',
                         'git+git://github.com/veg/hppy.git@0.9.6#egg=hppy-0.9.6'
