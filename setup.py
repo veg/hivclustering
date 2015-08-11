144e6da6ee4dd78ecc034a30157b63bbcde71753
Steven Weaver <sweaver@ucsd.edu>
2015-08-11 16:19:59 -0700

    bumping bioext dependency

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 sys.path.insert(0, join(split(abspath(__file__))[0], 'lib'))
 
 setup(name='hivclustering',
-      version="1.1.4",
+      version="1.1.5",
       description='HIV molecular clustering tools',
       author='Sergei Kosakovsky Pond',
       author_email='spond@ucsd.edu',
@@ -22,11 +22,11 @@ setup(name='hivclustering',
         'scripts/hivnetworkcsv',
     ],
     dependency_links = ['git+git://github.com/veg/hyphy-python.git@0.1.1#egg=HyPhy-0.1.1',
-                        'git+git://github.com/veg/BioExt.git@0.17.2#egg=BioExt-0.17.2',
+                        'git+git://github.com/veg/BioExt.git@0.17.2#egg=BioExt-0.17.3',
                         'git+git://github.com/veg/hppy.git@0.9.6#egg=hppy-0.9.6'
                        ],
     install_requires=[
-        'BioExt >= 0.17.2',
+        'BioExt >= 0.17.3',
         'HyPhy >= 0.1.1',
         'hppy >= 0.9.6',
         ],
