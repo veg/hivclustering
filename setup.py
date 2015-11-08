a8464ec44262e1240df3b119e657e1ac6737e0aa
Steven Weaver <sweaver@ucsd.edu>
2015-11-08 15:40:14 -0800

    0.2.1 version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 sys.path.insert(0, join(split(abspath(__file__))[0], 'lib'))
 
 setup(name='hivclustering',
-      version="1.2.0",
+      version="1.2.1",
       description='HIV molecular clustering tools',
       author='Sergei Kosakovsky Pond',
       author_email='spond@ucsd.edu',
@@ -22,7 +22,7 @@ setup(name='hivclustering',
         'scripts/hivnetworkcsv',
         'scripts/TNS'
     ],
-    dependency_links = ['git+git://github.com/veg/hyphy-python.git@0.1.1#egg=HyPhy-0.1.2',
+    dependency_links = ['git+git://github.com/veg/hyphy-python.git@0.1.2#egg=HyPhy-0.1.2',
                         'git+git://github.com/veg/BioExt.git@0.17.2#egg=BioExt-0.17.3',
                         'git+git://github.com/veg/hppy.git@0.9.6#egg=hppy-0.9.6'
                        ],
